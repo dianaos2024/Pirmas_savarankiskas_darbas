9311b57359e7597bdf8ac07cb5cd6979b6e975e9
dianaos2024 <diana.ostik@vvk.lt>
2025-09-30 21:46:41 +0300

    txt file function included

--- MultiFile/main.cpp
+++ MultiFile/main.cpp
@@ -4,21 +4,24 @@ int main() {
 try {
     vector<Studentas> grupe;
     int pasirinkimas;
-    std::cout << "1 - Rankinis ivedimas\n2 - Atsitiktinis generavimas\n";
+    std::cout << "1 - Rankinis ivedimas\n2 - Atsitiktinis generavimas\n3 - Nuskaitymas is failo\n";
     std::cin >> pasirinkimas;
 
     int kiek;
-    std::cout << "Kiek studentu norite sugeneruoti? ";
-    std::cin >> kiek;
+    int kiekNd;
 
     if (pasirinkimas == 1) {
+            std::cout << "Kiek studentu norite sugeneruoti? ";
+            std::cin >> kiek;
             for (int i = 0; i < kiek; i++) {
                 Studentas s;
                 std::cin >> s;
                 grupe.push_back(s);
             }
     } else if (pasirinkimas == 2) {
-            int kiekNd;
+            std::cout << "Kiek studentu norite sugeneruoti? ";
+            std::cin >> kiek;
+
             std::cout << "Kiek namu darbu pazymiu generuoti kiekvienam? ";
             std::cin >> kiekNd;
             for (int i = 0; i < kiek; i++) {
@@ -26,7 +29,16 @@ try {
                 s.generuotiAtsitiktinai(s, kiekNd);
                 grupe.push_back(s);
             }
+    } else if (pasirinkimas == 3) {
+            if (!nuskaitytiIsFailo("kursiokai.txt", grupe)) {
+                std::cerr << "Nepavyko nuskaityti failo.\n";
+                return 1;
+            }
     }
+
+    std::sort(grupe.begin(), grupe.end(), [](const Studentas& a, const Studentas& b) {
+            return a.getPavarde() < b.getPavarde();
+        });
 
     cout << "Pavarde       Vardas           Galutinis (Vid.) / Galutinis (Med.)\n";
     cout << "------------------------------------------------------------------\n";
