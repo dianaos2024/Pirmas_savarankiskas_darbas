a95d88e483c51c15c547ff13c27231396c3706b3
dianaos2024 <diana.ostik@vvk.lt>
2025-10-14 19:34:41 +0300

    Update MyLib.h

--- MultiFile/MyLib.h
+++ MultiFile/MyLib.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <string>
 #include <vector>
@@ -20,25 +21,27 @@ using std::right;
 using std::fixed;
 using std::setprecision;
 using std::sort;
-class Studentas{
+
+class Studentas {
     string vardas;
     string pavarde;
-    vector<int>paz;
+    vector<int> paz;
     int egzaminas;
     double rezultatas;
     double rezultatasMediana;
 public:
     Studentas();
-    Studentas(string A,string B, vector<int> C, int D);
+    Studentas(string A, string B, vector<int> C, int D);
     Studentas(const Studentas& other);
     Studentas& operator=(const Studentas& other);
+    ~Studentas();
     void rez();
     void rezMediana();
-    ~Studentas();
     friend std::istream& operator>>(std::istream& is, Studentas& s);
     friend std::ostream& operator<<(std::ostream& os, const Studentas& s);
     std::string getVardas() const { return vardas; };
     std::string getPavarde() const { return pavarde; };
+    double getRezultatas() const { return rezultatas; }
     void generuotiAtsitiktinai(Studentas& s, int nPazymiu = 5);
 };
 
